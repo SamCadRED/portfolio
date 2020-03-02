6135498da75daeee5c9e5a5fbcde3efe30fa9417
SamCadRED <60133200+SamCadRED@users.noreply.github.com>
2020-03-02 11:04:18 +0100

    Update activite8.php

--- pages/activite/activite8.php
+++ pages/activite/activite8.php
@@ -52,7 +52,7 @@
     </div>
 
     <div class='activite'>
-        <h1 class='acti_title'><?php echo $acti4 ?></h1>
+        <h1 class='acti_title'><?php echo $acti8 ?></h1>
         <?php 
             $description = "Création de composants graphiques et logiciels et intégration dans une application mobile.";
             $contexte = "Je l'ai effectué en équipe avec le graphiste de l'entreprise.";
