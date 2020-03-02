45ffdd7a7e76d58df59032e55ecb086c57eb1ef0
SamCadRED <60133200+SamCadRED@users.noreply.github.com>
2020-03-02 11:06:43 +0100

    Update activite10.php

--- pages/activite/activite10.php
+++ pages/activite/activite10.php
@@ -56,9 +56,9 @@
         <?php 
             $description = "Déploiement de Machines Virtuelles de développement sur un serveur local";
             $contexte = "Seul en entreprise";
-            $objectif = "Déployer 3 machines virtuelles (copie de serveurs deproduction) de test et développement.";
+            $objectif = "Déployer 3 machines virtuelles (copie de serveurs de production) de test et développement.";
             $env = "Windows 10, HyperV, Oracle et Framework .NET";
-            $travail = "Réaliser un Dump de la base de donnée sur leserveur de production, puis installer Oracle et notre application métier sur les VM pour qu'elles puissent accueillir les données ";
+            $travail = "Réaliser un Dump de la base de donnée sur le serveur de production, puis installer Oracle et notre application métier sur les VM pour qu'elles puissent accueillir les données ";
             $competence = "
                 A1.3.4 Déploiement d'un service
                 A3.2.1 Installation et configuration d'éléments d'infrastructure<br>
