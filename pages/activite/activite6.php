778f8c42bf7c5c51e0bff49a6ef0e9cd27c4dd9f
SamCadRED <60133200+SamCadRED@users.noreply.github.com>
2020-03-02 11:03:42 +0100

    Update activite6.php

--- pages/activite/activite6.php
+++ pages/activite/activite6.php
@@ -52,7 +52,7 @@
     </div>
 
     <div class='activite'>
-        <h1 class='acti_title'><?php echo $acti4 ?></h1>
+        <h1 class='acti_title'><?php echo $acti6 ?></h1>
         <?php 
             $description = "Mise en place d'un service de ticketing avec AWS et Axonaut";
             $contexte = "J'ai effectué ce travail seul en entreprise";
