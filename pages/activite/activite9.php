d26ff3e6b294019f9ee644ee7030f4bc5481624a
SamCadRED <60133200+SamCadRED@users.noreply.github.com>
2020-03-02 11:04:33 +0100

    Update activite9.php

--- pages/activite/activite9.php
+++ pages/activite/activite9.php
@@ -52,7 +52,7 @@
     </div>
 
     <div class='activite'>
-        <h1 class='acti_title'><?php echo $acti4 ?></h1>
+        <h1 class='acti_title'><?php echo $acti9 ?></h1>
         <?php 
             $description = "Mise en place d'une veille technologique grâce à différents outils";
             $contexte = "Seul en centre";
