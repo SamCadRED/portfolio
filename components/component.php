fe07786825d0e80d83710b31e695289422f545b2
SamCadRED <60133200+SamCadRED@users.noreply.github.com>
2020-03-02 14:34:23 +0100

    Update component.php

--- components/component.php
+++ components/component.php
@@ -35,9 +35,9 @@ $acti13 = "PPE équipement visiteurs (1ère année)";
 $acti14 = "PPE Léger (2ème année)";
 $acti15 = "PPE lourd (2ème année)";
 
-$wip = "<div style="padding: 10px; margin: 10px; text-align: center; border: solid var(--color-light) 1px;">
+$wip = '<div style="padding: 10px; margin: 10px; text-align: center; border: solid var(--color-light) 1px;">
           <h3>Work in Progress</h3>
-        </div>
+        </div>'
 
 function display_case($title, $second_title, $text) {
     return "<div class='case'>
