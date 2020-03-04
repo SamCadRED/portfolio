ae3473d9716c867f977451c5c052b88863551369
SamCadRED <samuelcadiou@icloud.com>
2020-03-04 19:47:18 +0100

    V2of Veille ready

--- veille.php
+++ veille.php
@@ -1,4 +1,6 @@
-<?php include_once "components/component.php" ?>
+<?php include_once "veille/component.php";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,51 +26,31 @@
 
     <div id="main_content">
 
-        <div class="rss">
-            <h2>Flux RSS</h2>
-            <div class="feed_cont">
-
-                <div class="google_feed">
-                    <h3>Google News</h3>
-                    <div class="rss_feed">
-                        <script src="//rss.bloople.net/?url=https%3A%2F%2Fnews.google.com%2Frss%2Fsearch%3Fq%3DMachine%2BLearning%26hl%3Den-US%26gl%3DUS%26ceid%3DUS%253Aen%26x%3D1571747125.1658&detail=-1&limit=20&showtitle=false&type=js"></script>
-                    </div>
-                </div>
-                
-                <hr>
-
-                <div class="ferme_feed">
-                    <h3>La Ferme du Web</h3>
-                    <div class="rss_feed">
-                        <script src="//rss.bloople.net/?url=https%3A%2F%2Fwww.lafermeduweb.net%2Ftag%2Frss%2Fmachinelearning.xml&detail=-1&limit=20&showtitle=false&type=js"></script>
-                    </div>
-                </div>
-
-                <div class="medium_feed">
-                    <h3>Medium</h3>
-                    <div class="rss_feed">
-                        <script src="//rss.bloople.net/?url=https%3A%2F%2Fmedium.com%2Ffeed%2Ftag%2Fdata-science&detail=-1&showtitle=false&type=js"></script>
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-
-        <hr>
-
-        <div class="websites">
-            <h2>Articles Presse / Site Web</h2>
+        <div class="rss_container">
+                <?php 
+                    $i = 0;
+                    while ($i < count($urlArray)) {
+                        $url = $urlArray[$i];
+                        if ($i < count($imgArray)) {
+                            $img = $imgArray[$i];
+                        } else {
+                            $img = "";
+                        }
+                        
+                        echo display_feed($url, $img);
+                        $i += 1;
+                    }
+                ?>
         </div>
 
         <hr>
 
         <div class="others">
-            <h2>Autres</h2>
             <div class="podcast">
                 <h3>Podcast</h3>
                 <div class="savant">
                     <iframe src="https://open.spotify.com/embed-podcast/episode/0wnHHEy63nmojdkkI4AHOF" width="100%" height="200" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-                    <!-- Spotify integration https://developer.spotify.com/documentation/widgets/generate/play-button/ -->
+                    <!-- Spotify integration https://developer.spotify.com/documentation/widgets/generate/play-button/  -->
                 </div>
             </div>
         </div>
@@ -76,7 +58,7 @@
     </div>
     
     <div class="footer_cont">
-        <?php echo $footer ?>
+        <?php $footer ?>
     </div>
 </body>
 </html>
---
+++ veille/component.php
@@ -0,0 +1,28 @@
+<?php 
+$footer = "<footer class='footer'>
+                <div class='top_footer'>
+                    <h4><a href='mailto:samuelcadiou@hotmail.fr'>Me contacter par mail</a></h4>
+                    <h4><a href='https://www.linkedin.com/in/samuel-cadiou-822759133' target='_blank'>LinkedIn</a></h4>
+                    <h4><a href='https://github.com/SamCadRED/portfolio' target='_blank'>GitHub</a></h4>
+                </div>
+                <p>©2020 par Samuel Cadiou</p>
+            </footer>";
+
+
+$urlArray = array(
+    "//rss.bloople.net/?url=https%3A%2F%2Fwww.lafermeduweb.net%2Ftag%2Frss%2Fmachinelearning.xml%3Fsearch_veille%255Bsorting%255D%3D%26search_veille%255BnbPerPages%255D%3D10%26search_veille%255BdateInterval%255D%3D%26search_veille%255Bauthor%255D%3D%26search_veille%255Blang%255D%3Dall%26search_veille%255Bsearch%255D%3D&limit=1&showtitle=false&type=js",
+    "//rss.bloople.net/?url=https%3A%2F%2Fnanonets.com%2Fblog%2Frss%2F&limit=1&showtitle=false&type=js",
+    "//rss.bloople.net/?url=http%3A%2F%2Fnews.mit.edu%2Frss%2Ftopic%2Fmachine-learning&limit=1&showtitle=false&type=js",
+    "//rss.bloople.net/?url=https%3A%2F%2Fmedium.com%2Ffeed%2Ftag%2Fmachine-learning&limit=1&showtitle=false&type=js",
+    
+);
+$imgArray = array("images/mao.jpg");
+
+
+function display_feed($url, $img) {
+    return "
+        <div class='feed_cont' 
+            style='background-image: linear-gradient(to bottom,rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6) ), url(".$img.")'>
+            <script src='".$url."'></script>
+        </div>";
+} 
